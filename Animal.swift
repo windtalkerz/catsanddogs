602861782049cf68152a8eb2a3df38fe94b4c2bc
Marc Ortlieb <windtalkerz@me.com>
2016-04-13 16:50:26 +0200

    + Animal

---
+++ Animal.swift
@@ -0,0 +1,13 @@
+//
+//  New.swift
+//  TestApp
+//
+//  Created by Marc Orlieb on 13.04.16.
+//  Copyright © 2016 Marc Orlieb. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    
+}
