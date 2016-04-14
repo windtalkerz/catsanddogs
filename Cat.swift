226ae4aaf7d98216b9e6a621d18f6f6d2629d264
Marc Ortlieb <windtalkerz@me.com>
2016-04-14 09:04:59 +0200

    +Cat

---
+++ Cat.swift
@@ -0,0 +1,19 @@
+//
+//  New.swift
+//  TestApp
+//
+//  Created by Marc Orlieb on 13.04.16.
+//  Copyright © 2016 Marc Orlieb. All rights reserved.
+//
+
+import Foundation
+
+class Cat {
+    var paws = 4
+    var face = "cute"
+    
+    func sleep() {
+        
+    }
+}
+
